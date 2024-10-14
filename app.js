3ac0c1120e87633727f665d60dfdb420400616bc
Raju Mannem <rajumannem71@gmail.com>
2024-10-15 02:00:52 +0530

    delete route added

--- app.js
+++ app.js
@@ -144,7 +144,7 @@ app.post(
         return new Promise((resolve, reject) => {
           blobStream.on('error', reject);
           blobStream.on('finish', async () => {
-            const publicUrl = `https://storage.googleapis.com/${bucket.name}/${blob.name}`;
+            const publicUrl = `https://firebasestorage.googleapis.com/v0/b/${bucket.name}/o/${encodeURIComponent(blob.name)}?alt=media`;
             resolve(publicUrl);
           });
           blobStream.end(file.buffer);
@@ -196,6 +196,15 @@ app.get("/api/profiles/:id", async (req, res) => {
     res.status(500).send({ error: error.message || "An error occurred" });
   }
 });
+
+app.delete("/api/profiles/:id", async (req, res) => {
+    try {
+      const profile = await Profiles.findByIdAndDelete(req.params.id);
+      res.status(200).json(profile);
+    } catch (error) {
+      res.status(500).send({ error: error.message || "An error occurred" });
+    }
+  });  
 
 app.listen(5000, () => {
   console.log("server is running on 5000");
