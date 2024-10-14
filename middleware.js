589e2dab92ec528085b336186dead7ab52691d3f
Raju Mannem <rajumannem71@gmail.com>
2024-10-14 22:51:06 +0530

    firebase admin update

--- middleware.js
+++ middleware.js
@@ -1,17 +1,26 @@
-import jwt from 'jsonwebtoken';
+import { OAuth2Client } from 'google-auth-library';
 
-export const auth = (req, res, next) => {
-  const token = req.headers['authorization']?.split(' ')[1];
+const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
 
+export const auth = async (req, res, next) => {
+  const token = req.body.token;
   if (!token) {
-    return res.status(401).json({ error: 'No token provided' });
+    return res.status(400).send({ error: "Token is required" });
   }
+  try {
+    const ticket = await client.verifyIdToken({
+      idToken: token,
+      audience: process.env.GOOGLE_CLIENT_ID,
+    });
 
-  jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
-    if (err) {
-      return res.status(401).json({ error: 'Unauthorized' });
-    }
-    req.userId = decoded.id; // Set user id in request
+    const { email, sub } = ticket.getPayload();
+    
+    // Optionally, you can set the user info on the request object
+    req.user = { email, id: sub };
+    
+    // Call the next middleware or route handler
     next();
-  });
+  } catch (error) {
+    res.status(401).send({ error: "Invalid token" });
+  }
 };
--- model.js
+++ model.js
@@ -1,9 +1,32 @@
 import mongoose from "mongoose";
 const userSchema = new mongoose.Schema({
-    id: { type: String, required: true, unique: true },
-    name: { type: String },
-    email: { type: String, required: true, unique: true },
-    picture: { type: String },
-  });
-const User=mongoose.model('user',userSchema);
-export default User;
+  email: { type: String, required: true },
+  googleId: { type: String, required: true },
+  access: { type: Boolean, default: false },
+  payment: { type: Boolean, default: false }
+});
+const adminSchema = new mongoose.Schema({
+  adminName: { type: String, required: true },
+  password: { type: String, required: true },
+});
+const profileSchema = new mongoose.Schema({
+  fullName: { type: String, required: true },
+  fatherName: { type: String, required: true },
+  motherName: { type: String, required: true },
+  mobile: { type: Number, required: true, unique: true },
+  age: { type: Number, required: true },
+  occupation: { type: String, required: true },
+  experience: { type: String, required: true },
+  salary: { type: String, required: true },
+  currentAddress: { type: String, required: true },
+  permanentAddress: { type: String, required: true },
+  photo1: { type: String},
+  photo2: { type: String},
+  height: { type: String, required: true },
+  message: { type: String }
+});
+
+const User = mongoose.model("user", userSchema);
+const Admin = mongoose.model("admins", adminSchema);
+const Profiles = mongoose.model("profiles", profileSchema);
+export { Admin, User, Profiles};
